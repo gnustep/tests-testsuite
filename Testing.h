15619fdf504308bfe448e3779b4722688e851c66
Richard Frith-Macdonald <rfm@gnu.org>
2005-07-24 08:52:44 +0000

    Conform to coding standards
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/Testsuite/trunk@21525 72102866-910b-0410-8b05-ffd578937521

--- Testing.h
+++ Testing.h
@@ -22,35 +22,35 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-static void pass(int testPassed,const char *description, ...) __attribute__ ((format(printf,2,3)));
-static void pass(int testPassed,const char *description, ...)
+static void pass(int testPassed, const char *description, ...) __attribute__ ((format(printf, 2, 3)));
+static void pass(int testPassed, const char *description, ...)
 {
-	va_list args;
-	va_start(args,description);
-	fprintf(stderr,testPassed?"PASS: ":"FAIL: ");
-	vfprintf(stderr,description,args);
-	fprintf(stderr,"\n");
-	va_end(args);
+  va_list args;
+  va_start(args, description);
+  fprintf(stderr, testPassed?"PASS: ":"FAIL: ");
+  vfprintf(stderr, description, args);
+  fprintf(stderr, "\n");
+  va_end(args);
 }
-static void unresolved(const char *description, ...) __attribute__ ((format(printf,1,2), unused));
+static void unresolved(const char *description, ...) __attribute__ ((format(printf, 1, 2), unused));
 static void unresolved(const char *description, ...)
 {
-	va_list args;
-	va_start(args,description);
-	fprintf(stderr,"UNRESOLVED: ");
-	vfprintf(stderr,description,args);
-	fprintf(stderr,"\n");
-	va_end(args);
+  va_list args;
+  va_start(args, description);
+  fprintf(stderr, "UNRESOLVED: ");
+  vfprintf(stderr, description, args);
+  fprintf(stderr, "\n");
+  va_end(args);
 }
-static void unsupported(const char *description, ...) __attribute__ ((format(printf,1,2), unused));
+static void unsupported(const char *description, ...) __attribute__ ((format(printf, 1, 2), unused));
 static void unsupported(const char *description, ...)
 {
-	va_list args;
-	va_start(args,description);
-	fprintf(stderr,"UNSUPPORTED: ");
-	vfprintf(stderr,description,args);
-	fprintf(stderr,"\n");
-	va_end(args);
+  va_list args;
+  va_start(args, description);
+  fprintf(stderr, "UNSUPPORTED: ");
+  vfprintf(stderr, description, args);
+  fprintf(stderr, "\n");
+  va_end(args);
 }
 
 /* Pass an object as a string to a print function.  */
@@ -59,11 +59,11 @@ static void unsupported(const char *description, ...)
 /* some good macros to compare floating point numbers */
 #include <math.h>
 #include <float.h>
-#define EQ(x,y) (fabs((x)-(y))<=fabs((x)+(y))*(FLT_EPSILON*100))
-#define LE(x,y) ((x)<(y) || EQ(x,y))
-#define GE(x,y) ((y)<(x) || EQ(x,y))
-#define LT(x,y) (!GE(x,y))
-#define GT(x,y) (!LE(x,y))
+#define EQ(x, y) (fabs((x) - (y)) <= fabs((x) + (y)) * (FLT_EPSILON * 100))
+#define LE(x, y) ((x)<(y) || EQ(x, y))
+#define GE(x, y) ((y)<(x) || EQ(x, y))
+#define LT(x, y) (!GE(x, y))
+#define GT(x, y) (!LE(x, y))
 
 #endif
 
