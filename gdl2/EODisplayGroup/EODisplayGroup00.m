ff7fa51a30b4910ae64ef0bae13956447dee4d49
Matt Rice <ratmice@gmail.com>
2006-09-26 14:51:22 +0000

    * gdl2/EODisplayGroup/EODisplayGroup00.m: add tests for         -selectedObject:, -selectNext:, -selectPrevious:,         -enabledToSetSelectedObjectValueForKey:, -clearSelection,         -lessThanQueryValues, -equalToQueryValues, 	-greaterThanQueryValues,         -qualifierFromQueryValues, -setValue:forObject:key:.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@23627 72102866-910b-0410-8b05-ffd578937521

--- gdl2/EODisplayGroup/EODisplayGroup00.m
+++ gdl2/EODisplayGroup/EODisplayGroup00.m
@@ -22,11 +22,31 @@
 */
 
 #include <Foundation/NSAutoreleasePool.h>
+#include <Foundation/NSCalendarDate.h>
 #include <EOControl/EOControl.h>
 #include <EOInterface/EOInterface.h>
-
+#include <EOControl/EOQualifier.h>
 #include "../GDL2Testing.h"
 
+@implementation EOKeyValueQualifier(equality)
+- (BOOL) isEqual:(id)obj
+{
+  if (![obj isKindOfClass: [self class]])
+    return NO;
+
+  return sel_eq(_selector, (SEL)((EOKeyValueQualifier *)obj)->_selector)
+         && [_key isEqual:((EOKeyValueQualifier *)obj)->_key]
+         && [_value isEqual:((EOKeyValueQualifier *)obj)->_value];
+}
+@end
+@implementation NSCalendarDate(equality)
+- (BOOL) isEqual:(id)obj
+{
+  if (![obj isKindOfClass:[self class]])
+    return NO;
+  return [[self description] isEqual: [obj description]];
+}
+@end
 int main(int argc,char **argv)
 {
   NSAutoreleasePool *pool = [NSAutoreleasePool new];
@@ -38,6 +58,7 @@ int main(int argc,char **argv)
   
   EOModel *model;
   EODataSource *dataSource;
+  NSCalendarDate *date;
 
   START_SET(YES);
 
@@ -90,12 +111,145 @@ int main(int argc,char **argv)
   [displayGroup insert: nil];
   result = [[displayGroup allObjects] count] == 1;
   result = result && [[displayGroup displayedObjects] count] == 1;
-  NSLog(@"%@",[displayGroup allObjects]);
   END_TEST(result, "-[EODisplayGroup insert:]");
+  
+  START_TEST(YES)
+  result = [displayGroup selectedObject] == [[displayGroup allObjects] objectAtIndex:0];
+  END_TEST(result, "-[EODisplayGroup selectedObject]");
+  
+  START_TEST(YES)
+  [displayGroup insert:nil]; 
+  [displayGroup insert:nil]; 
+  [displayGroup selectNext:nil];
+  [displayGroup selectNext:nil];
+  result = [displayGroup selectedObject] == [[displayGroup allObjects] objectAtIndex:2];
+  END_TEST(result, "-[EODisplayGroup selectNext:]");
+  
+  START_TEST(YES)
+  [displayGroup insert:nil]; 
+  [displayGroup selectPrevious:nil];
+  result = [displayGroup selectedObject] == [[displayGroup allObjects] objectAtIndex:1];
+  END_TEST(result, "-[EODisplayGroup selectPrevious:]");
+
+  START_TEST(YES)
+  result = [displayGroup enabledToSetSelectedObjectValueForKey:@"foo"] == YES;
+  END_TEST(result, "-[EODisplayGroup enabledToSetSelectedObjectValueForKey:] 1");
+
+  START_TEST(YES)
+  result = [displayGroup enabledToSetSelectedObjectValueForKey:@"@foo"] == YES;
+  END_TEST(result, "-[EODisplayGroup enabledToSetSelectedObjectValueForKey:] 2");
+
+  START_TEST(YES)
+  result = [displayGroup enabledToSetSelectedObjectValueForKey:@"@query<"];
+  END_TEST(result, "-[EODisplayGroup enabledToSetSelectedObjectValueForKey:] 3");
+
+  [displayGroup clearSelection];
+  START_TEST(YES)
+  result = [displayGroup selectedObject] == nil;
+  END_TEST(result, "-[EODisplayGroup clearSelection]");
+
+  START_TEST(YES)
+  result = [displayGroup enabledToSetSelectedObjectValueForKey:@"foo"] == NO;
+  END_TEST(result, "-[EODisplayGroup enabledToSetSelectedObjectValueForKey:] 4");
+
+  START_TEST(YES)
+  result = [displayGroup enabledToSetSelectedObjectValueForKey:@"@foo"] == NO;
+  END_TEST(result, "-[EODisplayGroup enabledToSetSelectedObjectValueForKey:] 5");
+  
+  START_TEST(YES)
+  result = [displayGroup enabledToSetSelectedObjectValueForKey:@"@query<"];
+  END_TEST(result, "-[EODisplayGroup enabledToSetSelectedObjectValueForKey:] 6"); 
+  [displayGroup selectNext:nil];
+	      
+  START_TEST(YES);
+  [displayGroup setValue:@"foo" forObject:nil key:@"@query<.name"];
+  result = [[[displayGroup lessThanQueryValues] objectForKey:@"name"] isEqual:@"foo"];
+  END_TEST(result, "-[EODisplayGroup lessThanQueryValues]");
+  
+  START_TEST(YES);
+  [displayGroup setValue:@"bar" forObject:nil key:@"@query>.name"];
+  result = [[[displayGroup greaterThanQueryValues] objectForKey:@"name"] isEqual:@"bar"];
+  END_TEST(result, "-[EODisplayGroup greaterThanQueryValues]");
+  
+  START_TEST(YES);
+  date = RETAIN([NSCalendarDate calendarDate]);
+  
+  [displayGroup setValue:[date description] forObject:nil key:@"@query=.date"];
+  result = [[[displayGroup equalToQueryValues] objectForKey:@"date"]
+	  	isEqual:[date description]];
+  END_TEST(result, "-[EODisplayGroup equalToQueryValues] 1");
 
   START_TEST(YES);
-  END_TEST(result, "-[EODisplayGroup ]");
+  [displayGroup setValue:@"hmm" forObject:nil key:@"@query=.name"];
+  result = [[[displayGroup equalToQueryValues] objectForKey:@"name"]
+                isEqual:@"hmm"];
+  END_TEST(result, "-[EODisplayGroup equalToQueryValues] 2");
+  
+  START_TEST(YES);
+  NS_DURING
+  [displayGroup setValue:@"bad" forObject:nil key:@"@query"];
+  result = NO;
+  NS_HANDLER
+  result = YES;
+  NS_ENDHANDLER
+  END_TEST(result, "-[EODisplayGroup setValue:forObject:key:] (bad query values)");
+  
+  START_TEST(YES);
+  NS_DURING
+  [displayGroup setValue:@"bad" forObject:nil key:@"@query<"];
+  result = NO;
+  NS_HANDLER
+  result = YES;
+  NS_ENDHANDLER
+  END_TEST(result, "-[EODisplayGroup setValue:forObject:key:] (bad query values)");
+  
+  START_TEST(YES);
+  NS_DURING
+  [displayGroup setValue:@"bad" forObject:nil key:@"@queryhmmm"];
+  result = NO;
+  NS_HANDLER
+  result = YES;
+  NS_ENDHANDLER
+  END_TEST(result, "-[EODisplayGroup setValue:forObject:key:] (bad query values)");
+  
+  { 
+    NSArray *arr;
+    EOAndQualifier *qual = [displayGroup qualifierFromQueryValues];
+    id tmpQual;
+    
+    arr = [qual qualifiers];
+    START_TEST(YES)
+    tmpQual = [EOKeyValueQualifier qualifierWithKey:@"date" 
+    				operatorSelector:EOQualifierOperatorEqual
+				value:date];
+    result = [arr containsObject: tmpQual];
+    END_TEST(result, "-[EODisplayGroup qualifierFromQueryValues] 1");
+    RELEASE(date);
+     
+    START_TEST(YES)
+    tmpQual = [EOKeyValueQualifier qualifierWithKey:@"name" 
+    				operatorSelector:EOQualifierOperatorGreaterThanOrEqualTo
+				value:@"bar"];
+    result = [arr containsObject: tmpQual];
+    END_TEST(result, "-[EODisplayGroup qualifierFromQueryValues] 2");
+    
+    [displayGroup setQueryOperatorValues:[NSDictionary dictionaryWithObject:@"!=" forKey:@"name"]];
+    START_TEST(YES)
+    tmpQual = [EOKeyValueQualifier qualifierWithKey:@"name" 
+    				operatorSelector:EOQualifierOperatorLessThanOrEqualTo
+				value:@"foo"];
+    result = [arr containsObject:tmpQual];
+    END_TEST(result, "-[EODisplayGroup qualifierFromQueryValues] 3");
+    
+    START_TEST(YES)
+    tmpQual = [EOKeyValueQualifier qualifierWithKey:@"name"
+                                operatorSelector:EOQualifierOperatorCaseInsensitiveLike
+                                value:@"hmm*"];
 
+    result = [arr containsObject:tmpQual];
+    END_TEST(result, "-[EODisplayGroup qualifierFromQueryValues] 4");
+  }
+  
   END_SET("EODisplayGroup/EODisplayGroup00.m");
 
   [pool release];
