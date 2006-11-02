7df406f91b9c17fcff2113594944bc13b8c07403
Richard Frith-MacDonald <rfm@gnu.org>
2006-11-02 14:12:06 +0000

    Generate better test files which produce testcase passes on success rather than just a failure on compile failure.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@24016 72102866-910b-0410-8b05-ffd578937521

--- SelfContainedHeaders/generate_header_check_files.sh
+++ SelfContainedHeaders/generate_header_check_files.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 # Quick hack to auto-generate all the header checking files.
 
-for I in /opt/GNUstep/System/Library/Headers/$1/*.h ; do
-	NAME=`basename $I`
+mkdir $1
+for I in /usr/GNUstep/System/Library/Headers/$1/*.h ; do
+  NAME=`basename $I`
 
-	# Don't check compatibility headers from the great header exodus.
-	if ! grep -l "#warning.*is now included" $I >/dev/null 2>&1 ; then
-		echo "#include <"$1"/"$NAME">" > $1/$NAME".m"
-		echo "int main(int argc,char **argv){return 0;}" >> $1/$NAME".m"
-	fi
+  # Don't check compatibility headers from the great header exodus.
+  if ! grep -l "#warning.*is now included" $I >/dev/null 2>&1 ; then
+    echo "#include <"$1"/"$NAME">" > $1/$NAME".m"
+    echo '#include "ObjectTesting.h"' >> $1/$NAME".m"
+    echo '@class NSAutoreleasePool;' >> $1/$NAME".m"
+    echo 'int main()' >> $1/$NAME".m"
+    echo '{' >> $1/$NAME".m"
+    echo '  NSAutoreleasePool *arp = [NSAutoreleasePool new];' >> $1/$NAME".m"
+    echo '' >> $1/$NAME".m"
+    echo "  pass (1, \"include of $1/$NAME works\");" >> $1/$NAME".m"
+    echo '  [arp release];' >> $1/$NAME".m"
+    echo '  return 0;' >> $1/$NAME".m"
+    echo '}' >> $1/$NAME".m"
+  fi
 done
 
