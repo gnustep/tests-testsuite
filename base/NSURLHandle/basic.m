47bd88c4b983f36f20f626bc12e31d2707050693
Graham J Lee <leeg@thaesofereode.info>
2007-01-25 11:27:45 +0000

    Basic NSURLHandle tests (class conformance and cache functionality)
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@24414 72102866-910b-0410-8b05-ffd578937521

---
+++ base/NSURLHandle/basic.m
@@ -0,0 +1,36 @@
+#import <Foundation/Foundation.h>
+#import "Testing.h"
+#import "ObjectTesting.h"
+
+int main()
+{
+  CREATE_AUTORELEASE_POOL(arp);
+  Class cls;
+  NSURL *httpURL, *foobarURL;
+  id handle1, handle2;
+
+  httpURL = [NSURL URLWithString: @"http://www.gnustep.org"];
+  foobarURL = [NSURL URLWithString: @"foobar://localhost/madeupscheme"];
+
+  TEST_FOR_CLASS(@"NSURLHandle", [NSURLHandle alloc],
+    "NSURLHandle +alloc returns an NSURLHandle");
+
+  cls = [NSURLHandle URLHandleClassForURL: httpURL];
+
+  pass([cls canInitWithURL: httpURL] == YES,
+    "Appropriate subclass found for +URLHandleClassForURL:");
+  handle1 = [[cls alloc] initWithURL: httpURL cached: YES];
+  handle2 = [NSURLHandle cachedHandleForURL: httpURL];
+
+  pass(handle2 != nil, "Available handle returned from cache");
+
+  [handle1 autorelease];
+  [cls autorelease];
+
+  cls = [NSURLHandle URLHandleClassForURL: foobarURL];
+
+  pass(cls == Nil, "Nil class returned for unknown URL scheme");
+
+  DESTROY(arp);
+  return 0;
+}
