0b95001e5e48e6908a4d4bd79c533b8436ef3a2e
Richard Frith-MacDonald <rfm@gnu.org>
2007-04-15 09:34:18 +0000

    Add a few tests for recent bugfix
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@25055 72102866-910b-0410-8b05-ffd578937521

--- base/NSBundle/resources.m
+++ base/NSBundle/resources.m
@@ -7,69 +7,79 @@
 
 int main()
 {
- CREATE_AUTORELEASE_POOL(arp);
- NSBundle *bundle;
- NSBundle *gstepBundle;
- NSArray  *arr;
+  CREATE_AUTORELEASE_POOL(arp);
+  NSBundle *bundle;
+  NSBundle *gstepBundle;
+  NSArray  *arr;
   
- bundle = [NSBundle bundleWithPath: [[NSFileManager defaultManager] 
- 						currentDirectoryPath]];
- gstepBundle = [NSBundle bundleForLibrary: @"gnustep-base"];
+  bundle = [NSBundle bundleWithPath: [[NSFileManager defaultManager] 
+    currentDirectoryPath]];
+  gstepBundle = [NSBundle bundleForLibrary: @"gnustep-base"];
  
- pass([bundle isKindOfClass:[NSBundle class]],"+bundleWithPath returns anNSBundle");
- arr = [bundle pathsForResourcesOfType:@"m" inDirectory:nil];
- pass([arr isKindOfClass:[NSArray class]] && [arr count],"-pathsForResourcesOfType:inDirectory: returns an array");
- pass([bundle pathForResource:@"hiwelf0-2" 
-                             ofType:nil 
-			inDirectory:nil] == nil,
-      "-pathForResource:ofType:inDirectory: works with nil args");
- pass([bundle pathForResource:@"hiwelf0-2" ofType:nil] == nil,
-      "-pathForResource:ofType: works with nil args");
- pass([[bundle resourcePath] testEquals: [[bundle bundlePath] 
-                                 stringByAppendingPathComponent:@"Resources"]],
-      "-resourcePath returns the correct path");
+  pass([bundle isKindOfClass: [NSBundle class]],
+    "+bundleWithPath returns anNSBundle");
+  arr = [bundle pathsForResourcesOfType: @"m" inDirectory: nil];
+  pass([arr isKindOfClass: [NSArray class]] && [arr count],
+    "-pathsForResourcesOfType: inDirectory: returns an array");
+  pass([bundle pathForResource: @"hiwelf0-2" 
+		        ofType: nil 
+		   inDirectory: nil] == nil,
+    "-pathForResource:ofType:inDirectory: works with nil args");
+  pass([bundle pathForResource: @"hiwelf0-2" ofType: nil] == nil,
+    "-pathForResource:ofType: works with nil type");
+  pass([bundle pathForResource: nil ofType: @"tiff"] == nil,
+    "-pathForResource:ofType: works with nil name");
+  pass([bundle pathForResource: @"hiwelf0-2" ofType: @""] == nil,
+    "-pathForResource:ofType: works with empty type");
+  pass([bundle pathForResource: @"" ofType: @"tiff"] == nil,
+    "-pathForResource:ofType: works with empty name");
+  pass([[bundle resourcePath] testEquals: [[bundle bundlePath] 
+    stringByAppendingPathComponent: @"Resources"]],
+    "-resourcePath returns the correct path");
  
- pass([[NSBundle pathForResource:@"abbreviations" 
-                          ofType:@"plist" 
-			  inDirectory: [[gstepBundle bundlePath] 
-			     stringByAppendingPathComponent:@"NSTimeZones"]]
-			      testForString],
-      "+pathForResource:ofType:inDirectory: works");
+  pass([[NSBundle pathForResource: @"abbreviations" 
+                           ofType: @"plist" 
+		      inDirectory: [[gstepBundle bundlePath] 
+    stringByAppendingPathComponent: @"NSTimeZones"]] testForString],
+    "+pathForResource:ofType:inDirectory: works");
  
- pass([[NSBundle pathForResource:@"abbreviations" 
-                          ofType:@"plist" 
-		     inDirectory:[[gstepBundle bundlePath] 
-			    stringByAppendingPathComponent:@"NSTimeZones"]
-	             withVersion:0] testForString],
-      "+pathForResource:ofType:inDirectory:withVersion: works");
+  pass([[NSBundle pathForResource: @"abbreviations" 
+                           ofType: @"plist" 
+		      inDirectory: [[gstepBundle bundlePath] 
+    stringByAppendingPathComponent: @"NSTimeZones"] withVersion: 0]
+    testForString],
+    "+pathForResource:ofType:inDirectory:withVersion: works");
 
- arr = [gstepBundle pathsForResourcesOfType:@"m" 
-                                 inDirectory:@"NSTimeZones"];
- pass(([arr isKindOfClass:[NSArray class]] && [arr count] > 0),
-      "-pathsForResourcesOfType:inDirectory: returns an array");
- pass([[gstepBundle pathForResource:@"abbreviations"
-                            ofType:@"plist"
-		       inDirectory:@"NSTimeZones"] testForString],
-      "-pathForResource:ofType:inDirectory: finds a file");
- pass([gstepBundle pathForResource:@"abbreviations"
-                            ofType:@"8nicola8"
-		       inDirectory:@"NSTimeZones"] == nil,
-      "-pathForResource:ofType:inDirectory: doesn't find a non-existing file");
- pass([gstepBundle pathForResource:@"abbreviations"
-                            ofType:@"plist"
-		       inDirectory:@"NSTimeZones_dummy"] == nil,
-      "-pathForResource:ofType:inDirectory: doesn't find files in a non-existing dir");
- pass([[gstepBundle pathForResource:@"abbreviations"
-                             ofType:nil
-		        inDirectory:@"NSTimeZones"] testForString],
-      "-pathForResource:ofType:inDirectory: with nil type finds a file");
- pass([gstepBundle pathForResource:@"whasssdlkf"
-                             ofType:nil
-		        inDirectory:@"NSTimeZones"] == nil,
-      "-pathForResource:ofType:inDirectory: with nil type doesn't find non-existing files");
+  arr = [gstepBundle pathsForResourcesOfType: @"m" 
+                                 inDirectory: @"NSTimeZones"];
+  pass(([arr isKindOfClass: [NSArray class]] && [arr count] > 0),
+    "-pathsForResourcesOfType:inDirectory: returns an array");
+  pass([[gstepBundle pathForResource: @"abbreviations"
+			      ofType: @"plist"
+			 inDirectory: @"NSTimeZones"] testForString],
+    "-pathForResource:ofType:inDirectory: finds a file");
+  pass([gstepBundle pathForResource: @"abbreviations"
+                             ofType: @"8nicola8"
+		        inDirectory: @"NSTimeZones"] == nil,
+    "-pathForResource:ofType:inDirectory: doesn't find a non-existing file");
+  pass([gstepBundle pathForResource: @"abbreviations"
+                             ofType: @"plist"
+		        inDirectory: @"NSTimeZones_dummy"] == nil,
+    "-pathForResource:ofType:inDirectory: doesn't find files in"
+    "a non-existing dir");
+  pass([[gstepBundle pathForResource: @"abbreviations"
+                              ofType: nil
+		         inDirectory: @"NSTimeZones"] testForString],
+    "-pathForResource:ofType:inDirectory: with nil type finds a file");
+  pass([gstepBundle pathForResource: @"whasssdlkf"
+                             ofType: nil
+		        inDirectory: @"NSTimeZones"] == nil,
+    "-pathForResource:ofType:inDirectory: with nil type doesn't find"
+    "non-existing files");
  
- pass([[gstepBundle pathForResource:@"NSTimeZones" ofType:nil] testForString], 
-      "-pathForResource:ofType: finds a file");
- DESTROY(arp);
- return 0;
+  pass([[gstepBundle pathForResource: @"NSTimeZones" ofType: nil]
+    testForString], 
+    "-pathForResource:ofType: finds a file");
+  DESTROY(arp);
+  return 0;
 }
