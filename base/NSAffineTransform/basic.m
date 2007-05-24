4c2c8fb2ea30b7a3e4f05470f46b36e3d22aae99
Richard Frith-MacDonald <rfm@gnu.org>
2007-05-24 18:29:57 +0000

    Add some simple tests
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@25192 72102866-910b-0410-8b05-ffd578937521

--- base/NSAffineTransform/basic.m
+++ base/NSAffineTransform/basic.m
@@ -34,6 +34,19 @@ int main()
   pass(p.x == 10 && p.y == 10 && s.width == 10 && s.height == 10,
     "flip is reversible");
   
+  testObj = [NSAffineTransform transform];
+  [testObj translateXBy: 5.0 yBy: 6.0];
+  p = [testObj transformPoint: NSMakePoint(10,10)];
+  pass(p.x == 15.0 && p.y == 16.0, "simple translate works");
+
+  [testObj translateXBy: 5.0 yBy: 4.0];
+  p = [testObj transformPoint: NSMakePoint(10,10)];
+  pass(p.x == 20.0 && p.y == 20.0, "two simple translates work");
+  
+  [testObj rotateByDegrees: 90.0];
+  p = [testObj transformPoint: NSMakePoint(10,10)];
+  pass(p.x == 0.0 && p.y == 20.0, "translate and rotate works");
+  
   DESTROY(arp);
   return 0;
 }
