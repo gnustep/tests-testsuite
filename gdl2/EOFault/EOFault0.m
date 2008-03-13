05d84498274168557c073fec528590edf1fd5160
David Ayers <ayers@fsfe.org>
2008-03-13 15:32:57 +0000

    * gdl2/EOFault/EOFault0.m: New EOFault tests to insure fault doesn't fire.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@26297 72102866-910b-0410-8b05-ffd578937521

--- gdl2/EOFault/EOFault0.m
+++ gdl2/EOFault/EOFault0.m
@@ -1,6 +1,5 @@
-#include <Foundation/NSString.h>
-#include <EOControl/EOFault.h>
-#include <Foundation/NSAutoreleasePool.h>
+#import <Foundation/Foundation.h>
+#import <EOControl/EOControl.h>
 #include "../../Testing.h"
 
 @interface TestFaultHandler : EOFaultHandler
@@ -20,6 +19,7 @@ int main()
   int rc1 = [foo retainCount];
   int rc2;
   int i;
+  Class fClass = [foo class];
   
   [EOFault makeObjectIntoFault:foo withHandler:fh];
   pass([EOFault isFault:foo], "EOFault +isFault:");
@@ -48,8 +48,24 @@ int main()
   pass(rc1 - 1 == rc2, "EOFault -release %i %i", rc1, rc2);
   rc1 = rc2;
   
+  pass([foo isKindOfClass:[NSMutableString class]] && [EOFault isFault:foo], "-isKindOfClass: is transparent but doesn't fire");
+  pass([foo isMemberOfClass:fClass]&& [EOFault isFault:foo], "-isMemberOfClass: is transparent but doesn't fire");
+  pass([[foo class] isSubclassOfClass: [NSString class]] && [EOFault isFault:foo], "-class is transparent but doesn't fire");
+  pass([[foo superclass] isSubclassOfClass: [NSObject class]] && [EOFault isFault:foo], "-superclass is transparent but doesn't fire");
+  pass([foo conformsToProtocol: @protocol(NSCopying)] && [EOFault isFault:foo], "-conformsToProtocol is transparent but doesn't fire");
+  pass([foo isProxy] == NO && [EOFault isFault:foo], "-isProxy is transparent but doesn't fire");
+  pass([foo methodSignatureForSelector:@selector(appendString:)] && [EOFault isFault:foo], "-methodSignatureForSelector is transparent but doesn't fire");
+  pass([foo respondsToSelector:@selector(appendString:)] && [EOFault isFault:foo], "-respondsToSelector is transparent but doesn't fire");
+  pass([foo zone] && [EOFault isFault:foo], "zone is transparent but doesn't fire");
+  NS_DURING
+    [foo doesNotRecognizeSelector: @selector(addObject:)];
+  NS_HANDLER;
+  NS_ENDHANDLER;
+  pass([EOFault isFault:foo], "doesNotRecognizeSelector: is transparent but doesn't fire");
+
   [foo self];
-  pass([foo isKindOfClass:[NSMutableString class]], "fault fires");
+  pass([EOFault isFault:foo] == NO, "fault fire");
+
   pass([foo isEqual:@"foo"], "data intact");
  
   
