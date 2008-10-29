fe37188cfea186f9f171f7dfeb2362a9fe293afa
Richard Frith-MacDonald <rfm@gnu.org>
2008-10-29 07:09:19 +0000

    Add some simple numeric conversion tests
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@26990 72102866-910b-0410-8b05-ffd578937521

---
+++ base/NSString/test07.m
@@ -0,0 +1,30 @@
+#import "ObjectTesting.h"
+#import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSString.h>
+
+int main()
+{
+  CREATE_AUTORELEASE_POOL(arp);
+  double d;
+
+  pass([@"12" intValue] == 12, "simple intValue works");
+  pass([@"-12" intValue] == -12, "negative intValue works");
+  pass([@"+12" intValue] == 12, "positive intValue works");
+  pass([@"1.2" intValue] == 1, "intValue ignores trailing data");
+  pass([@"                                12" intValue] == 12,
+    "intValue with leading space works");
+
+  d = [@"1.2" doubleValue];
+  pass(d > 1.199999 && d < 1.200001, "simple doubleValue works");
+  d = [@"-1.2" doubleValue];
+  pass(d < -1.199999 && d > -1.200001, "negative doubleValue works");
+  d = [@"+1.2" doubleValue];
+  pass(d > 1.199999 && d < 1.200001, "positive doubleValue works");
+  d = [@"+1.2 x" doubleValue];
+  pass(d > 1.199999 && d < 1.200001, "doubleValue ignores trailing data");
+  d = [@"                                1.2" doubleValue];
+  pass(d > 1.199999 && d < 1.200001, "doubleValue with leading space works");
+
+  DESTROY(arp);
+  return 0;
+}
