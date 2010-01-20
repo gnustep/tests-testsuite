080a515555e27446e2a47b95b520781756b8d3a3
Richard Frith-MacDonald <rfm@gnu.org>
2010-01-20 09:44:20 +0000

    more info
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@29325 72102866-910b-0410-8b05-ffd578937521

--- base/NSTask/Helpers/processgroup.m
+++ base/NSTask/Helpers/processgroup.m
@@ -12,9 +12,12 @@ main(int argc, char **argv)
   for (i = 0; i < argc; i++)
     printf("argv[%d] %s\n", i, argv[i]);
   printf("pgrp %d\n", getpgrp());
+  printf("result of open of /dev/tty is %d\n", open("/dev/tty", O_RDONLY));
 */
   if (atoi(argv[1]) == getpgrp())
     i = 1;
+  else
+    i = 0;
 #endif  /* __MINGW32__ */
 
   [arp release];
