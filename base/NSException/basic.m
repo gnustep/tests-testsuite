0814dcd3ad4b3da15cbc0c3a7da6c1c78b545589
Richard Frith-MacDonald <rfm@gnu.org>
2010-01-24 06:32:53 +0000

    new test for NSException
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@29378 72102866-910b-0410-8b05-ffd578937521

---
+++ base/NSException/basic.m
@@ -0,0 +1,52 @@
+#import <Foundation/NSException.h>
+#import <Foundation/NSAutoreleasePool.h>
+#import "ObjectTesting.h"
+
+@interface      MyClass : NSObject
++ (void) testAbc;
+@end
+@implementation MyClass
++ (void) testAbc
+{
+  [NSException raise: NSGenericException format: @"In MyClass" userInfo: nil];
+}
+@end
+
+int main()
+{
+  NSException *obj;
+  NSMutableArray *testObjs = [[NSMutableArray alloc] init];
+  NSString *str;
+  NSAutoreleasePool   *arp = [NSAutoreleasePool new];
+
+  test_alloc_only(@"NSException"); 
+  obj = [NSException exceptionWithName: NSGenericException
+                                reason: nil
+                              userInfo: nil];
+  pass((obj != nil), "can create an exception");
+  pass(([[obj name] isEqualToString: NSGenericException]), "name works");
+  obj = [NSException exceptionWithName: NSGenericException
+                                reason: nil
+                              userInfo: nil];
+  [testObjs addObject: obj];
+  test_NSObject(@"NSException", testObjs);
+  
+  NS_DURING
+    [MyClass testAbc];
+  NS_HANDLER
+    {
+      NSArray *a = [localException callStackSymbols];
+      NSEnumerator *e = [a objectEnumerator];
+      NSString  *s = nil;
+
+//NSLog(@"%@ %@", localException, a);
+
+      while ((s = [e nextObject]) != nil)
+        if ([s rangeOfString: @"testAbc"].length > 0)
+          break;
+      pass(s != nil, "working callStackSymbols");
+    }
+  NS_ENDHANDLER
+  [arp release]; arp = nil;
+  return 0;
+}
