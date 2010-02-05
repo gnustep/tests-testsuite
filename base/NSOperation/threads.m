e3acf131e2feeffcc0117694616f167e19b9aa43
Richard Frith-MacDonald <rfm@gnu.org>
2010-02-05 11:41:02 +0000

    more tests
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@29479 72102866-910b-0410-8b05-ffd578937521

---
+++ base/NSOperation/threads.m
@@ -0,0 +1,145 @@
+#import <Foundation/NSOperation.h>
+#import <Foundation/NSThread.h>
+#import <Foundation/NSNotification.h>
+#import <Foundation/NSAutoreleasePool.h>
+#import "ObjectTesting.h"
+
+
+@interface      ThreadCounter : NSObject
+{
+  unsigned count;
+}
+- (unsigned) count;
+- (void) increment: (NSNotification*)n;
+- (void) reset;
+@end
+
+@implementation ThreadCounter
+- (unsigned) count
+{
+  return count;
+}
+
+- (void) dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver: self];
+  [super dealloc];
+}
+
+- (void) increment: (NSNotification*)n
+{
+  count++;
+}
+
+- (id) init
+{
+  [[NSNotificationCenter defaultCenter] addObserver: self
+                                           selector: @selector(increment:)
+                                               name: NSThreadWillExitNotification
+                                             object: nil];
+  return self;
+}
+
+- (void) reset
+{
+  count = 0;
+}
+
+@end
+
+@interface      OpFlag : NSOperation
+{
+  BOOL flag;
+}
+- (void) main;
+- (BOOL) ran;
+@end
+
+@implementation OpFlag
+- (void) main
+{
+  flag = YES;
+}
+- (BOOL) ran
+{
+  return flag;
+}
+@end
+
+@interface OpExit : OpFlag
+@end
+
+@implementation OpExit 
+- (void) main
+{
+  [super main];
+  [NSThread exit];
+}
+@end
+
+@interface OpRaise : OpFlag
+@end
+
+@implementation OpRaise 
+- (void) main
+{
+  [super main];
+  [NSException raise: NSGenericException format: @"done"];
+}
+@end
+
+int main()
+{
+  ThreadCounter         *cnt;
+  id                    obj;
+  NSAutoreleasePool     *arp = [NSAutoreleasePool new];
+
+  cnt = [ThreadCounter new];
+  pass((cnt != nil && [cnt count] == 0), "counter was set up");
+
+  // Check that operation runs in current thread.
+  obj = [OpFlag new];
+  [cnt reset];
+  [obj start];
+  pass(([obj ran] == YES), "operation ran");
+  pass(([cnt count] == 0), "operation ran in this thread");
+  [obj release];
+
+  // Check that monitoring of another thread works.
+  obj = [OpFlag new];
+  [cnt reset];
+  [NSThread detachNewThreadSelector: @selector(start)
+                           toTarget: obj
+                         withObject: nil];
+  sleep(1);
+  pass(([cnt count] == 1), "operation ran in other thread");
+  pass(([obj isFinished] == YES), "operation finished");
+  pass(([obj ran] == YES), "operation ran");
+  [obj release];
+
+  // Check that exit from thread in -main causes operation tracking to fail.
+  obj = [OpExit new];
+  [cnt reset];
+  [NSThread detachNewThreadSelector: @selector(start)
+                           toTarget: obj
+                         withObject: nil];
+  sleep(1);
+  pass(([cnt count] == 1), "operation ran in other thread");
+  pass(([obj isFinished] == NO), "operation exited");
+  pass(([obj ran] == YES), "operation ran");
+  pass(([obj isExecuting] == YES), "operation seems to be running");
+  [obj release];
+
+  // Check that raising exception in -main causes operation tracking to fail.
+  obj = [OpRaise new];
+  TEST_EXCEPTION([obj start];,
+  		 NSGenericException, YES, 
+		 "NSOperation exceptions propogate from main");
+  pass(([obj isFinished] == NO), "operation failed to finish");
+  pass(([obj ran] == YES), "operation ran");
+  pass(([obj isExecuting] == YES), "operation seems to be running");
+  [obj release];
+
+  [arp release]; arp = nil;
+  return 0;
+}
