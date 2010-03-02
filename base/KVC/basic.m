d8e5bbf1ccd504379ef803ad4356d331105a1770
Richard Frith-MacDonald <rfm@gnu.org>
2010-03-02 12:21:02 +0000

    check retain/release behavior
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@29818 72102866-910b-0410-8b05-ffd578937521

--- base/KVC/basic.m
+++ base/KVC/basic.m
@@ -7,42 +7,42 @@
 
 @interface TestClass : NSObject
 {
-  NSString * name;
-  NSDate * date;
+  NSString *name;
+  NSDate *date;
   int num1;
   double num2;
   int num3;
   int num4;
-  TestClass * child;
+  TestClass *child;
 }
 
-- (void)setNum3:(int)num;
-- (int)num3;
-- (void)_setNum4:(int)num;
-- (int)_num4;
+- (void) setNum3:(int) num;
+- (int) num3;
+- (void) _setNum4:(int) num;
+- (int) _num4;
 
 @end
 
 @implementation TestClass
 
-- (void)setNum3:(int)num
+- (void) setNum3:(int) num
 {
   num3 = num;
   if (num3 == 8) num3 = 7;
 }
 
-- (int)num3
+- (int) num3
 {
   return num3;
 }
 
-- (void)_setNum4:(int)num
+- (void) _setNum4:(int) num
 {
   num4 = num;
   if (num4 == 8) num4 = 7;
 }
 
-- (int)_num4
+- (int) _num4
 {
   return num4;
 }
@@ -51,28 +51,28 @@
 @interface UndefinedKey : NSObject
 {
   int num1;
-  NSString * string;
+  NSString *string;
 }
 @end
 
 @implementation UndefinedKey
-- (void)dealloc
+- (void) dealloc
 {
   [string release];
   [super dealloc];
 }
 
-- (void)setValue:(id)value forUndefinedKey:(NSString *)key
+- (void) setValue:(id) value forUndefinedKey:(NSString *) key
 {
-  if ([key isEqualToString:@"Lücke"]) {
+  if ([key isEqualToString: @"Lücke"]) {
     [string release];
     string = [value copy];
   }
 }
 
-- (id)valueForUndefinedKey:(NSString *)key
+- (id) valueForUndefinedKey:(NSString *) key
 {
-  if ([key isEqualToString:@"Lücke"]) {
+  if ([key isEqualToString: @"Lücke"]) {
     return string;
   }
 }
@@ -82,94 +82,106 @@
 @interface UndefinedKey2 : NSObject
 {
   int num1;
-  NSString * string;
+  NSString *string;
 }
 @end
 
 @implementation UndefinedKey2
-- (void)dealloc
+- (void) dealloc
 {
   [string release];
   [super dealloc];
 }
 
-- (void)handleTakeValue:(id)value forUnboundKey:(NSString *)key
+- (void) handleTakeValue:(id) value forUnboundKey:(NSString *) key
 {
-  if ([key isEqualToString:@"Lücke"]) {
+  if ([key isEqualToString: @"Lücke"]) {
     [string release];
     string = [value copy];
   }
 }
 
-- (id)handleQueryWithUnboundKey:(NSString *)key
+- (id) handleQueryWithUnboundKey:(NSString *) key
 {
-  if ([key isEqualToString:@"Lücke"]) {
+  if ([key isEqualToString: @"Lücke"]) {
     return string;
   }
 }
 
 @end
 
-int main()
+int main() 
 {
-  NSAutoreleasePool   *arp = [NSAutoreleasePool new];
+  NSAutoreleasePool *arp = [NSAutoreleasePool new];
+  NSMutableString *m = [NSMutableString string];
+  TestClass *tester = [[[TestClass alloc] init] autorelease];
+  NSUInteger rc;
 
-  TestClass * tester = [[[TestClass alloc] init] autorelease];
-  [tester setValue:[[[TestClass alloc] init] autorelease] forKey:@"child"];
-  UndefinedKey * undefinedKey = [[[UndefinedKey alloc] init] autorelease];
-  UndefinedKey2 * undefinedKey2 = [[[UndefinedKey2 alloc] init] autorelease];
+  [m appendString: @"testing"];
 
-  NSNumber * n = [NSNumber numberWithInt:8];
-  NSNumber * adjustedN = [NSNumber numberWithInt:7];
-  NSNumber * n2 = [NSNumber numberWithDouble:87.999];
+  [tester setValue:[[[TestClass alloc] init] autorelease] forKey: @"child"];
+  UndefinedKey *undefinedKey = [[[UndefinedKey alloc] init] autorelease];
+  UndefinedKey2 *undefinedKey2 = [[[UndefinedKey2 alloc] init] autorelease];
 
-  [tester setValue:@"tester" forKey:@"name"];
-  pass([[tester valueForKey:@"name"] isEqualToString:@"tester"],
-      "KVC works with strings");
-  [tester setValue:n forKey:@"num1"];
-  pass([[tester valueForKey:@"num1"] isEqualToNumber:n],
-      "KVC works with ints");
-  [tester setValue:n2 forKey:@"num2"];
-  pass([[tester valueForKey:@"num2"] isEqualToNumber:n2],
-      "KVC works with doubles");
+  NSNumber *n = [NSNumber numberWithInt:8];
+  NSNumber *adjustedN = [NSNumber numberWithInt:7];
+  NSNumber *n2 = [NSNumber numberWithDouble:87.999];
 
-  [tester setValue:n forKey:@"num3"];
-  pass([[tester valueForKey:@"num3"] isEqualToNumber:adjustedN],
-      "KVC works with setKey:");
-  [tester setValue:n forKey:@"num4"];
-  pass([[tester valueForKey:@"num4"] isEqualToNumber:adjustedN],
-      "KVC works with _setKey:");
+  [tester setValue: @"tester" forKey: @"name"];
+  pass([[tester valueForKey: @"name"] isEqualToString: @"tester"],
+      "KVC works with strings") ;
 
-  [undefinedKey setValue:@"GNUstep" forKey:@"Lücke"];
-  pass([[undefinedKey valueForKey:@"Lücke"] isEqualToString:@"GNUstep"],
-      "KVC works with undefined keys");
+  rc = [m retainCount];
+  [tester setValue: m forKey: @"name"];
+  pass([tester valueForKey: @"name"] == m,
+      "KVC works with mutable string") ;
+  pass(rc + 1 == [m retainCount], "KVC retains object values");
 
-  [undefinedKey2 setValue:@"GNUstep" forKey:@"Lücke"];
-  pass([[undefinedKey2 valueForKey:@"Lücke"] isEqualToString:@"GNUstep"],
-      "KVC works with undefined keys (using deprecated methods)");
+  [tester setValue:n forKey: @"num1"];
+  pass([[tester valueForKey: @"num1"] isEqualToNumber:n],
+      "KVC works with ints") ;
+  [tester setValue:n2 forKey: @"num2"];
+  pass([[tester valueForKey: @"num2"] isEqualToNumber:n2],
+      "KVC works with doubles") ;
+
+  [tester setValue:n forKey: @"num3"];
+  pass([[tester valueForKey: @"num3"] isEqualToNumber:adjustedN],
+      "KVC works with setKey:") ;
+  [tester setValue:n forKey: @"num4"];
+  pass([[tester valueForKey: @"num4"] isEqualToNumber:adjustedN],
+      "KVC works with _setKey:") ;
+
+  [undefinedKey setValue: @"GNUstep" forKey: @"Lücke"];
+  pass([[undefinedKey valueForKey: @"Lücke"] isEqualToString: @"GNUstep"],
+      "KVC works with undefined keys") ;
+
+  [undefinedKey2 setValue: @"GNUstep" forKey: @"Lücke"];
+  pass([[undefinedKey2 valueForKey: @"Lücke"] isEqualToString: @"GNUstep"],
+      "KVC works with undefined keys (using deprecated methods) ") ;
 
   TEST_EXCEPTION(
-    [tester setValue:@"" forKey:@"nonexistent"],
+    [tester setValue: @"" forKey: @"nonexistent"],
     NSUndefinedKeyException, YES,
-    "KVC properly throws @\"NSUnknownKeyException\"");
+    "KVC properly throws @\"NSUnknownKeyException\"") ;
 
   TEST_EXCEPTION(
-    [tester setValue:@"" forKey:@"nonexistent"],
+    [tester setValue: @"" forKey: @"nonexistent"],
     NSUndefinedKeyException, YES,
-    "KVC properly throws NSUndefinedKeyException");
+    "KVC properly throws NSUndefinedKeyException") ;
 
   TEST_EXCEPTION(
-    [tester setValue:@"" forKeyPath:@"child.nonexistent"],
+    [tester setValue: @"" forKeyPath: @"child.nonexistent"],
     @"NSUnknownKeyException", YES,
-    "KVC properly throws @\"NSUnknownKeyException\" with key paths");
+    "KVC properly throws @\"NSUnknownKeyException\" with key paths") ;
 
   TEST_EXCEPTION(
-    [tester setValue:@"" forKeyPath:@"child.nonexistent"],
+    [tester setValue: @"" forKeyPath: @"child.nonexistent"],
     NSUndefinedKeyException, YES,
-    "KVC properly throws NSUndefinedKeyException with key paths");
+    "KVC properly throws NSUndefinedKeyException with key paths") ;
 
-  pass(sel_getUid(0) == 0, "null string gives null selector");
-  pass(strcmp(sel_getName(0), "<null selector>") == 0, "null selector name");
+  pass(sel_getUid(0) == 0, "null string gives null selector") ;
+  pass(sel_registerName(0) == 0, "register null string gives null selector") ;
+  pass(strcmp(sel_getName(0) , "<null selector>") == 0, "null selector name") ;
 
   [arp release]; arp = nil;
   return 0;
