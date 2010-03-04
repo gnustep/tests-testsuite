489f17a747f696fefa114c2df5d0feb69bad1f4c
Richard Frith-MacDonald <rfm@gnu.org>
2010-03-04 07:29:12 +0000

    simple method list checks
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@29826 72102866-910b-0410-8b05-ffd578937521

--- base/Functions/runtime.m
+++ base/Functions/runtime.m
@@ -3,6 +3,7 @@
 #import <Foundation/NSException.h>
 #import <Foundation/NSDebug.h>
 #import <Foundation/NSObject.h>
+#import <Foundation/NSString.h>
 
 #include        <objc/runtime.h>
 
@@ -35,13 +36,33 @@
 }
 @end
 
+@interface      SubClass1 (Cat1)
+- (BOOL) catMethod;
+- (const char *) sel2;
+@end
+
+@implementation SubClass1 (Cat1)
+- (BOOL) catMethod
+{
+  return YES;
+}
+- (const char *) sel2
+{
+  return "category sel2";
+}
+@end
+
 int
 main(int argc, char *argv[])
 {
   Class         cls;
   Ivar          ivar;
+  unsigned int  count;
+  unsigned int  index;
+  Method        *methods;
 
   cls = [SubClass1 class];
+
   ivar = class_getInstanceVariable(cls, 0);
   pass(ivar == 0, "class_getInstanceVariable() returns 0 for null name");
   ivar = class_getInstanceVariable(cls, "bad name");
@@ -54,7 +75,10 @@ main(int argc, char *argv[])
   pass(ivar != 0, "class_getInstanceVariable() works for superclass ivar");
   ivar = class_getInstanceVariable(cls, "ivar1obj");
   pass(ivar != 0, "class_getInstanceVariable() works for superclass obj ivar");
-  NSLog(@"%s", ivar_getTypeEncoding(ivar));
+
+  methods = class_copyMethodList(cls, &count);
+  pass(count == 3, "SubClass1 has three methods");
+  pass(methods[count] == 0, "method list is terminated");
 
   cls = objc_allocateClassPair([NSString class], "runtime generated", 0);
   pass(cls != Nil, "can allocate a class pair");
