3e2c6f9fd294997d5f1c57ead8c7f8dd2ce6ec6b
Richard Frith-MacDonald <rfm@gnu.org>
2010-06-15 10:46:02 +0000

    add test for valueForKey in array
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@30731 72102866-910b-0410-8b05-ffd578937521

--- base/KVC/array.m
+++ base/KVC/array.m
@@ -1,16 +1,41 @@
 #import "ObjectTesting.h"
 #import <Foundation/Foundation.h>
 
+@interface ArrayIVar : NSObject
+{
+  NSArray *_testArray;
+}
+- (void) setTestArray: (NSArray*)array;
+- (NSArray*) testArray;
+@end
+
+@implementation ArrayIVar
+- (void) setTestArray: (NSArray*)array
+{
+  [array retain];
+  [_testArray release];
+  _testArray = array;
+}
+- (NSArray*) testArray
+{
+  return _testArray;
+}
+@end
+
 int main(int argc,char **argv)
 {
   NSAutoreleasePool *pool = [NSAutoreleasePool new];
-
   volatile BOOL result = NO;
-  id tmp = nil;
+  NSDictionary  *root;
+  NSArray       *array;
+  NSArray       *ivar;
+  ArrayIVar     *aiv;
+  NSString      *plist;
+  id            tmp = nil;
 
-  NSArray *array = [@"({value=10;},{value=12;})" propertyList];
-  NSString *plist = @"{displayGroup={allObjects=({detailArray=({value=4;},{value=2;});},{detailArray=({value=8;},{value=10;});});};}";
-  NSDictionary *root = [plist propertyList];
+  array = [@"({value=10;},{value=12;})" propertyList];
+  plist = @"{displayGroup={allObjects=({detailArray=({value=4;},{value=2;});},{detailArray=({value=8;},{value=10;});});};}";
+  root = [plist propertyList];
 
   result = [[array valueForKeyPath:@"@sum.value"] intValue] == 22;
   pass(result, "-[NSArray valueForKeyPath: @\"@sum.value\"]");
@@ -21,6 +46,12 @@ int main(int argc,char **argv)
   result = [[array valueForKeyPath:@"@count"] intValue] == 2;
   pass(result, "-[NSArray valueForKeyPath: @\"@count\"]");
 
+  aiv = [ArrayIVar new];
+  ivar = [NSArray arrayWithObjects: @"Joe", @"Foo", @"Bar", @"Cat", nil];
+  [aiv setTestArray: ivar];
+
+  pass([aiv valueForKeyPath: @"testArray.@count"]
+    == [ivar valueForKey: @"@count"], "valueForKey: matches valueForKeypath:");
 
   /* Advanced KVC */
 
