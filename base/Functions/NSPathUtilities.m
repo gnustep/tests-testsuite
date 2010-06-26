3e329bd9b713311f7a0a70b7f7fb18879d2f60c2
Richard Frith-MacDonald <rfm@gnu.org>
2010-06-26 06:18:29 +0000

    improve logging
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@30862 72102866-910b-0410-8b05-ffd578937521

--- base/Functions/NSPathUtilities.m
+++ base/Functions/NSPathUtilities.m
@@ -4,14 +4,27 @@
 int main()
 { 
   NSAutoreleasePool   *pool = [NSAutoreleasePool new];
+  id	o;
 
-  pass([NSUserName() length] > 0, "we can get a user name");
-  pass([NSFullUserName() length] > 0, "we can get a full user name");
-  pass([NSHomeDirectory() length] > 0, "we can get a home directory");
-  pass([NSStandardApplicationPaths() count] > 0, "we have app paths");
-  NSLog(@"NSStandardApplicationPaths() gives %@", NSStandardApplicationPaths());
-  pass([NSStandardLibraryPaths() count] > 0, "we have app paths");
-  NSLog(@"NSStandardLibraryPaths() gives %@", NSStandardLibraryPaths());
+  /* The path utilities provide information about the current user and
+   * the current filesystem layout ... and are inherently system dependent
+   * so we can't know if they are operating correctly.  The best we can do
+   * are trivial checks to see if their results look sane.
+   */
+  NSLog(@"NSUserName() %@", o = NSUserName());
+  pass([o length] > 0, "we can get a user name");
+  NSLog(@"NSFullUserName() %@", o = NSFullUserName());
+  pass([o length] > 0, "we can get a full user name");
+  NSLog(@"NSHomeDirectory() %@", o = NSHomeDirectory());
+  pass([o length] > 0, "we can get a home directory");
+  NSLog(@"NSTemporaryDirectory() %@", o = NSTemporaryDirectory());
+  pass([o length] > 0, "we can get a temporary directory");
+  NSLog(@"NSOpenStepRootDirectory() %@", o = NSOpenStepRootDirectory());
+  pass([o length] > 0, "we can get a root directory");
+  NSLog(@"NSStandardApplicationPaths() %@", o = NSStandardApplicationPaths());
+  pass([o count] > 0, "we have application paths");
+  NSLog(@"NSStandardLibraryPaths() %@", o = NSStandardLibraryPaths());
+  pass([o count] > 0, "we have library paths");
 
   [pool release]; pool = nil;
  
