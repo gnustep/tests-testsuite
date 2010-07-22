6d0b56f6df8aa7f14536beffe1c246f856ca9539
Richard Frith-MacDonald <rfm@gnu.org>
2010-07-22 05:48:28 +0000

    add subclass tests
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@31017 72102866-910b-0410-8b05-ffd578937521

--- base/NSProxy/test00.m
+++ base/NSProxy/test00.m
@@ -4,15 +4,48 @@
 #import <Foundation/NSProxy.h>
 #import <Foundation/NSString.h>
 
+@interface MyString : NSString
+{
+  id    _remote;
+}
+@end
+
 @interface MyProxy : NSProxy
 {
-  id _remote;
+  id    _remote;
 }
+@end
 
+@implementation MyString
+- (id) init
+{
+  _remote = nil;
+  return self;
+}
+- (void) dealloc
+{
+  [_remote release];
+}
+- (unichar) characterAtIndex: (NSUInteger)i
+{
+  return [_remote characterAtIndex: i];
+}
+- (NSUInteger) length
+{
+  return [_remote length];
+}
+- (void) setRemote:(id)remote
+{
+  ASSIGN(_remote,remote);
+}
+- (id) remote
+{
+  return _remote;
+}
 @end
 
 @implementation MyProxy
--(id) init
+- (id) init
 {
   _remote = nil;
   return self;
@@ -35,15 +68,15 @@
   else
     return [super isEqual: other];
 }
--(void) setRemote:(id)remote
+- (void) setRemote:(id)remote
 {
   ASSIGN(_remote,remote);
 }
--(NSString *) description
+- (NSString *) description
 {
   return [_remote description];
 }
--(id) remote
+- (id) remote
 {
   return _remote;
 }
@@ -68,6 +101,7 @@ int main()
   Class theClass = NSClassFromString(@"NSProxy");
   id obj = nil;
   id rem = @"Remote";
+  id sub = nil;
   
   pass(theClass == [NSProxy class], "uses +class to return self");
   pass([[NSProxy alloc] isProxy] == YES,
@@ -76,15 +110,32 @@ int main()
   obj = [[MyProxy alloc] init];
   pass(obj != nil, "Can create a MyProxy instance");
   pass([obj isEqual: obj], "proxy isEqual: to self without remote");
-  [obj setRemote:rem];
+  [obj setRemote: rem];
   pass([obj remote] == rem, "Can set the remote object for the proxy");
-  pass([obj length] == [rem length], "Can get the length of the remote object");
+  sub = [[MyString alloc] init];
+  pass(sub != nil, "Can create a MyString instance");
+  [sub setRemote: rem];
+  pass([sub remote] == rem, "Can set the remote object for the subclass");
+  pass([obj length] == [rem length], "Get the length of the remote object");
+  pass([sub length] == [rem length], "Get the length of the subclass object");
   pass([obj isEqual: rem], "proxy isEqual: to remote");
+  pass([obj isEqual: sub], "proxy isEqual: to subclass");
+  pass([sub isEqual: rem], "subclass isEqual: to remote");
+  pass([sub isEqual: obj], "subclass isEqual: to proxy");
   pass([rem isEqual: obj], "remote isEqual: to proxy");
+  pass([rem isEqual: sub], "remote isEqual: to subclass");
   pass([obj isEqualToString: rem], "proxy isEqualToString: to remote");
+  pass([obj isEqualToString: sub], "proxy isEqualToString: to subclass");
+  pass([sub isEqualToString: rem], "subclass isEqualToString: to remote");
+  pass([sub isEqualToString: obj], "subclass isEqualToString: to proxy");
   pass([rem isEqualToString: obj], "remote isEqualToString: to proxy");
+  pass([rem isEqualToString: sub], "remote isEqualToString: to subclass");
   pass([obj compare: rem] == NSOrderedSame, "proxy compare: remote");
+  pass([obj compare: sub] == NSOrderedSame, "proxy compare: subclass");
+  pass([sub compare: rem] == NSOrderedSame, "subclass compare: remote");
+  pass([sub compare: obj] == NSOrderedSame, "subclass compare: proxy");
   pass([rem compare: obj] == NSOrderedSame, "remote compare: proxy");
+  pass([rem compare: sub] == NSOrderedSame, "remote compare: subclass");
   
   [arp release]; arp = nil;
   return 0;
