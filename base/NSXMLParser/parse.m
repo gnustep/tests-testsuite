ae3e688c130a0674cbd181b4addbdf230d489faf
Richard Frith-MacDonald <rfm@gnu.org>
2010-12-20 17:51:48 +0000

    extend a little
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@31751 72102866-910b-0410-8b05-ffd578937521

--- base/NSXMLParser/parse.m
+++ base/NSXMLParser/parse.m
@@ -3,34 +3,34 @@
 #import <Foundation/NSXMLParser.h>
 #include <string.h>
 
-@interface      Handler : NSObject <NSXMLParserDelegate>
+@interface      Handler : NSObject
 {
   NSMutableString       *s;
 }
 
-- (void) parser: (NSXMLParser *)parser
-  didStartMappingPrefix: (NSString *)prefix
-  toURI: uri;
-  
-- (void) parser: (NSXMLParser *)parser
-  didEndMappingPrefix: (NSString *)prefix;
-  
-- (void) parser: (NSXMLParser *)parser
-  foundCDATA: (NSString *)string;
+- (void)parserDidStartDocument: (NSXMLParser *)parser;
+- (void)parserDidEndDocument: (NSXMLParser *)parser;
+- (void)parser: (NSXMLParser *)parser foundNotationDeclarationWithName: (NSString *)name publicID: (NSString *)publicID systemID: (NSString *)systemID;
+- (void)parser: (NSXMLParser *)parser foundUnparsedEntityDeclarationWithName: (NSString *)name publicID: (NSString *)publicID systemID: (NSString *)systemID notationName: (NSString *)notationName;
+- (void)parser: (NSXMLParser *)parser foundAttributeDeclarationWithName: (NSString *)attributeName forElement: (NSString *)elementName type: (NSString *)type defaultValue: (NSString *)defaultValue;
+- (void)parser: (NSXMLParser *)parser foundElementDeclarationWithName: (NSString *)elementName model: (NSString *)model;
 
-- (void) parser: (NSXMLParser *)parser
-  foundCharacters: (NSString *)string;
+- (void)parser: (NSXMLParser *)parser foundInternalEntityDeclarationWithName: (NSString *)name value: (NSString *)value;
 
-- (void) parser: (NSXMLParser *)parser
-  didStartElement: (NSString *)elementName
-  namespaceURI: (NSString *)namespaceURI
-  qualifiedName: (NSString *)qualifiedName
-  attributes: (NSDictionary *)attributeDict;
+- (void)parser: (NSXMLParser *)parser foundExternalEntityDeclarationWithName: (NSString *)name publicID: (NSString *)publicID systemID: (NSString *)systemID;
 
-- (void) parser: (NSXMLParser *)parser
-  didEndElement: (NSString *)elementName
-  namespaceURI: (NSString *)namespaceURI
-  qualifiedName: (NSString *)qName;
+- (void)parser: (NSXMLParser *)parser didStartElement: (NSString *)elementName namespaceURI: (NSString *)namespaceURI qualifiedName: (NSString *)qName attributes: (NSDictionary *)attributeDict;
+- (void)parser: (NSXMLParser *)parser didEndElement: (NSString *)elementName namespaceURI: (NSString *)namespaceURI qualifiedName: (NSString *)qName;
+- (void)parser: (NSXMLParser *)parser didStartMappingPrefix: (NSString *)prefix toURI: (NSString *)namespaceURI;
+- (void)parser: (NSXMLParser *)parser didEndMappingPrefix: (NSString *)prefix;
+- (void)parser: (NSXMLParser *)parser foundCharacters: (NSString *)string;
+- (void)parser: (NSXMLParser *)parser foundIgnorableWhitespace: (NSString *)whitespaceString;
+- (void)parser: (NSXMLParser *)parser foundProcessingInstructionWithTarget: (NSString *)target data: (NSString *)data;
+- (void)parser: (NSXMLParser *)parser foundComment: (NSString *)comment;
+- (void)parser: (NSXMLParser *)parser foundCDATA: (NSData *)CDATABlock;
+- (NSData *)parser: (NSXMLParser *)parser resolveExternalEntityName: (NSString *)name systemID: (NSString *)systemID;
+- (void)parser: (NSXMLParser *)parser parseErrorOccurred: (NSError *)parseError;
+- (void)parser: (NSXMLParser *)parser validationErrorOccurred: (NSError *)validationError;
 @end
 
 @implementation Handler
@@ -52,39 +52,81 @@
   return self;
 }
 
-- (void) parser: (NSXMLParser *)parser
-  didStartMappingPrefix: (NSString *)prefix
-  toURI: uri
+- (void) parserDidStartDocument: (NSXMLParser *)parser
 {
-  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd), prefix];
+  [s appendFormat: @"%@\n", NSStringFromSelector(_cmd)];
 }
-  
-- (void) parser: (NSXMLParser *)parser
-  didEndMappingPrefix: (NSString *)prefix
+
+- (void) parserDidEndDocument: (NSXMLParser *)parser
 {
-  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd), prefix];
-}
-  
-- (void) parser: (NSXMLParser *)parser
-  foundCDATA: (NSString *)string
-{
-  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd), string];
+  [s appendFormat: @"%@\n", NSStringFromSelector(_cmd)];
 }
 
 - (void) parser: (NSXMLParser *)parser
-  foundCharacters: (NSString *)string
+  foundNotationDeclarationWithName: (NSString *)name
+  publicID: (NSString *)publicID
+  systemID: (NSString *)systemID
 {
-  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd), string];
+  [s appendFormat: @"%@ %@ %@ %@\n", NSStringFromSelector(_cmd), name,
+    publicID, systemID];
 }
+
+- (void) parser: (NSXMLParser *)parser
+  foundUnparsedEntityDeclarationWithName: (NSString *)name
+  publicID: (NSString *)publicID
+  systemID: (NSString *)systemID
+  notationName: (NSString *)notationName
+{
+  [s appendFormat: @"%@ %@ %@ %@ %@\n", NSStringFromSelector(_cmd), name,
+    publicID, systemID, notationName];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  foundAttributeDeclarationWithName: (NSString *)attributeName
+  forElement: (NSString *)elementName
+  type: (NSString *)type
+  defaultValue: (NSString *)defaultValue
+{
+  [s appendFormat: @"%@ %@ %@ %@ %@\n", NSStringFromSelector(_cmd),
+    attributeName, elementName, type, defaultValue];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  foundElementDeclarationWithName: (NSString *)elementName
+  model: (NSString *)model
+{
+  [s appendFormat: @"%@ %@ %@\n", NSStringFromSelector(_cmd),
+    elementName, model];
+}
+
+
+- (void) parser: (NSXMLParser *)parser
+  foundInternalEntityDeclarationWithName: (NSString *)name
+  value: (NSString *)value
+{
+  [s appendFormat: @"%@ %@ %@\n", NSStringFromSelector(_cmd),
+    name, value];
+}
+
+
+- (void) parser: (NSXMLParser *)parser
+  foundExternalEntityDeclarationWithName: (NSString *)name
+  publicID: (NSString *)publicID
+  systemID: (NSString *)systemID
+{
+  [s appendFormat: @"%@ %@ %@ %@\n", NSStringFromSelector(_cmd),
+    name, publicID, systemID];
+}
+
 
 - (void) parser: (NSXMLParser *)parser
   didStartElement: (NSString *)elementName
   namespaceURI: (NSString *)namespaceURI
-  qualifiedName: (NSString *)qualifiedName
+  qualifiedName: (NSString *)qName
   attributes: (NSDictionary *)attributeDict
 {
   [s appendFormat: @"%@ %@ %@ %@ %@\n", NSStringFromSelector(_cmd),
-    elementName, namespaceURI, qualifiedName, attributeDict];
+    elementName, namespaceURI, qName, attributeDict];
 }
 
 - (void) parser: (NSXMLParser *)parser
@@ -93,7 +135,81 @@
   qualifiedName: (NSString *)qName
 {
   [s appendFormat: @"%@ %@ %@ %@\n", NSStringFromSelector(_cmd),
-    elementName, namespaceURI, qName];
+  elementName, namespaceURI, qName];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  didStartMappingPrefix: (NSString *)prefix
+  toURI: (NSString *)namespaceURI
+{
+  [s appendFormat: @"%@ %@ %@\n", NSStringFromSelector(_cmd),
+  prefix, namespaceURI];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  didEndMappingPrefix: (NSString *)prefix
+{
+  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd),
+    prefix];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  foundCharacters: (NSString *)string
+{
+  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd),
+    string];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  foundIgnorableWhitespace: (NSString *)whitespaceString
+{
+  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd),
+    whitespaceString];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  foundProcessingInstructionWithTarget: (NSString *)target
+  data: (NSString *)data
+{
+  [s appendFormat: @"%@ %@ %@\n", NSStringFromSelector(_cmd),
+    target, data];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  foundComment: (NSString *)comment
+{
+  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd),
+    comment];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  foundCDATA: (NSData *)CDATABlock
+{
+  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd),
+    CDATABlock];
+}
+
+- (NSData *) parser: (NSXMLParser *)parser
+  resolveExternalEntityName: (NSString *)name
+  systemID: (NSString *)systemID
+{
+  [s appendFormat: @"%@ %@ %@\n", NSStringFromSelector(_cmd),
+    name, systemID];
+  return nil;
+}
+
+- (void) parser: (NSXMLParser *)parser
+  parseErrorOccurred: (NSError *)parseError
+{
+  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd),
+    parseError];
+}
+
+- (void) parser: (NSXMLParser *)parser
+  validationErrorOccurred: (NSError *)validationError
+{
+  [s appendFormat: @"%@ %@\n", NSStringFromSelector(_cmd),
+    validationError];
 }
 
 - (void) reset
@@ -152,7 +268,7 @@ int main()
   const char            *x1 = "<?xml version=\"1.0\"?><test></test>";
   const char            *x1e = "<test></test>";
   NSString              *e1 =
-@"parser:didStartElement:namespaceURI:qualifiedName:attributes: test  test {\n}\nparser:didEndElement:namespaceURI:qualifiedName: test  test\n";
+@"parserDidStartDocument:\nparser:didStartElement:namespaceURI:qualifiedName:attributes: test  test {\n}\nparser:didEndElement:namespaceURI:qualifiedName: test  test\nparserDidEndDocument:\n";
 
   pass(testParse(x1, e1), "simple document 1");
   pass(testParse(x1e, e1), "simple document 1");
