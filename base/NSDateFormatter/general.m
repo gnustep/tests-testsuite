8af8d7f6da7dad504c811602b04b5997a5e8a80b
Stefan Bidigaray <stefanbidi@gmail.com>
2011-01-28 00:27:54 +0000

    Really add the file this time.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@31964 72102866-910b-0410-8b05-ffd578937521

---
+++ base/NSDateFormatter/general.m
@@ -0,0 +1,62 @@
+#import <Foundation/Foundation.h>
+#import "Testing.h"
+#import "ObjectTesting.h"
+
+int main(void)
+{
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  NSDateFormatter *inFmt;
+  NSDateFormatter *outFmt;
+  NSDate *date;
+  NSString *str;
+  NSLocale *locale;
+  NSCalendar *cal;
+  unsigned int components;
+  NSInteger year;
+  
+  [NSTimeZone setDefaultTimeZone: [NSTimeZone timeZoneWithName: @"GMT"]];
+  
+  inFmt = [[NSDateFormatter alloc] init];
+  [inFmt setDateFormat: @"yyyy-MM-dd 'at' HH:mm"];
+  date = [inFmt dateFromString: @"2011-01-27 at 17:36"];
+  outFmt = [[NSDateFormatter alloc] init];
+  [outFmt setLocale: [[NSLocale alloc] initWithLocaleIdentifier: @"pt_BR"]];
+  [outFmt setDateFormat: @"HH:mm 'on' EEEE MMMM d"];
+  str = [outFmt stringFromDate: date];
+  passeq (str, @"17:36 on quinta-feira janeiro 27",
+    "Output has the same format as Cocoa.");
+  RELEASE(outFmt);
+  RELEASE(inFmt);
+  
+  locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_GB"];
+  inFmt = [NSDateFormatter new];
+  [inFmt setDateStyle: NSDateFormatterShortStyle];
+  [inFmt setTimeStyle: NSDateFormatterNoStyle];
+  [inFmt setLocale: locale];
+  [inFmt setTimeZone: [NSTimeZone timeZoneWithName: @"GMT"]];
+  date = [inFmt dateFromString: @"15/06/1982"];
+  passeq ([date description], @"1982-06-15 00:00:00 +0000",
+    "GMT time zone is correctly accounted for.");
+  [inFmt setTimeZone: [NSTimeZone timeZoneWithName: @"EST"]];
+  date = [inFmt dateFromString: @"15/06/1982"];
+  passeq ([date description], @"1982-06-15 05:00:00 +0000",
+    "EST time zone is correctly accounted for.");
+  RELEASE(inFmt);
+  
+  cal = [[NSCalendar alloc] initWithCalendarIdentifier: NSGregorianCalendar];
+  [cal setTimeZone: [NSTimeZone timeZoneWithName: @"CST"]];
+  [cal setLocale: locale];
+  components = NSYearCalendarUnit;
+  year = [[cal components: components fromDate: date] year];
+  str = [NSDateFormatter localizedStringFromDate: date
+          dateStyle: NSDateFormatterLongStyle
+          timeStyle: NSDateFormatterNoStyle];
+  pass (year == 1982, "Year is 1982");
+  passeq (str, @"June 15, 1982", "Date is formatted correctly.");
+  RELEASE(locale);
+  RELEASE(cal);
+  
+  RELEASE(pool);
+  return 0;
+}
+
