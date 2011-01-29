49ff430885e2ba2bafaa0e3a130d1e6131c9522d
Richard Frith-MacDonald <rfm@gnu.org>
2011-01-29 16:08:15 +0000

    test for notifications
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/testsuite/trunk@31974 72102866-910b-0410-8b05-ffd578937521

--- base/NSUserDefaults/general.m
+++ base/NSUserDefaults/general.m
@@ -1,11 +1,31 @@
 #import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSNotification.h>
 #import <Foundation/NSUserDefaults.h>
 #import "ObjectTesting.h"
+
+@interface      Observer : NSObject
+{
+  unsigned count;
+}
+- (NSString*) count;
+- (void) notified: (NSNotification*)n;
+@end
+
+@implementation Observer
+- (NSString*) count
+{
+  return [NSString stringWithFormat: @"%u", count];
+}
+- (void) notified: (NSNotification*)n
+{
+  count++;
+}
+@end
 
 int main()
 {
   NSAutoreleasePool   *arp = [NSAutoreleasePool new];
-
+  Observer *obs = [[Observer new] autorelease];
   NSUserDefaults *defs;
 
   defs = [NSUserDefaults standardUserDefaults];
@@ -31,13 +51,22 @@ int main()
 }
 #endif
 
+  [[NSNotificationCenter defaultCenter] addObserver: obs
+    selector: @selector(notified:)
+    name: NSUserDefaultsDidChangeNotification
+    object: nil];
+
   [defs setBool: YES forKey: @"Test Suite Bool"];
   pass([defs boolForKey: @"Test Suite Bool"],
        "NSUserDefaults can set/get a BOOL");
 
+  passeq([obs count], @"1", @"setting a boolean causes notification");
+
   [defs setInteger: 34 forKey: @"Test Suite Int"];
   pass([defs integerForKey: @"Test Suite Int"] == 34,
        "NSUserDefaults can set/get an int");
+
+  passeq([obs count], @"2", @"setting an integer causes notification");
 
   [defs setObject: @"SetString" forKey: @"Test Suite Str"];
   pass([[defs stringForKey: @"Test Suite Str"] isEqual: @"SetString"],
